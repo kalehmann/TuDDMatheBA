c3577fa465df638c7d4e2eef0b459c22bc4b3144
Karsten Lehmann <mail@kalehmann.de>
2021-06-05 22:36:58 +0200

    doc: stringmod

--- courses/pr20/homework/11/stringmod.f95
+++ courses/pr20/homework/11/stringmod.f95
@@ -1,5 +1,8 @@
 !! Albina Oscherowa
 !! Karsten Lehmann
+
+!! Haelt eine Zeichenkette mit 80 Zeichen als Datenstrom mit
+!! aktueller Leseposition
 MODULE stringmod
   IMPLICIT NONE
 
@@ -10,12 +13,14 @@ MODULE stringmod
   CHARACTER(LEN=80) :: data
   INTEGER(KIND=1) :: index
 CONTAINS
+  !! Liesst die maximal 80 Zeichen lange Kette ein
   SUBROUTINE getnextchar(c)
     CHARACTER(LEN=1), INTENT(OUT) :: c
     c = data(index:index)
     index = index + 1
   END SUBROUTINE getnextchar
 
+  !! Gibt das naechste Zeichen der Kette zurueck
   SUBROUTINE readstring()
     READ(*,'(A)') data
     index = 1
